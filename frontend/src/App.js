0580d46cdecfc5ba8d920c6291e923cf333215d1
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-11 10:59:13 +0530

    code added

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+    gopi
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Verified.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Verified = () => {
+  return (
+    <div>Verified</div>
+  )
+}
+
+export default Verified
