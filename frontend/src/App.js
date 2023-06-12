f32da9a1c07945eabbc89107f5b4b4bf78902736
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-12 09:06:48 +0530

    code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Navbar from "./Route/Navbar";
 import MainRoute from "./Route/MainRoute";
+import Auth from "./Authentication/Auth";
 
 
 function App() {
   return (
-      <Flex>
-        <Box w='22%'>
-          <Navbar />
-        </Box>
-        <Box w='48%'>
-          <MainRoute />
-        </Box>
-        <Box w='30%' display={{base:'none',md:'none',lg:'block'}}>
-          <Navbar />
-        </Box>
-      </Flex>
+      // <Flex>
+      //   <Box w='22%'>
+      //     <Navbar />
+      //   </Box>
+      //   <Box w='48%'>
+      //     <MainRoute />
+      //   </Box>
+      //   <Box w='30%' display={{base:'none',md:'none',lg:'block'}}>
+      //     <Navbar />
+      //   </Box>
+      // </Flex>
+      <Box>
+        <Auth />
+      </Box>
   );
 }
 
---
+++ frontend/src/Authentication/Auth.js
@@ -0,0 +1,24 @@
+import { Box, Button, Flex } from '@chakra-ui/react'
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <Box>
+        <Flex>
+            <Box w='50%'>
+hum
+            </Box> 
+            <Box w='50%'>
+<Button>
+    google
+</Button>
+<Button>
+    facebook
+</Button>
+            </Box>
+        </Flex>
+    </Box>
+  )
+}
+
+export default Auth
