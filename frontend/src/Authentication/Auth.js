a5eee6f3bb1ef501d2ce25aeb37201afcb65ecaf
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-12 09:10:24 +0530

    code added

--- frontend/src/Authentication/Auth.js
+++ frontend/src/Authentication/Auth.js
@@ -4,19 +4,7 @@ import React from 'react'
 const Auth = () => {
   return (
     <Box>
-        <Flex>
-            <Box w='50%'>
-hum
-            </Box> 
-            <Box w='50%'>
-<Button>
-    google
-</Button>
-<Button>
-    facebook
-</Button>
-            </Box>
-        </Flex>
+
     </Box>
   )
 }
