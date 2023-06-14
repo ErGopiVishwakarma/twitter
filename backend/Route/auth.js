ce0f6aeff5a957a0e417a1bc1526783a7bc64068
Gopi vishwakarma <g2468vi@gmail.com>
2023-06-14 17:42:47 +0530

    code added

--- backend/Route/auth.js
+++ backend/Route/auth.js
@@ -40,7 +40,7 @@ authRouter.get("/login/success", async (req, res) => {
 			res.status(401).json({ error: true, message: error.message })
 		}
 	} else {
-		
+		res.status(403).json({ error: true, message: "Not Authorized" });
 	}
 });
 
